9017576e0010b61c51f070a27bed9d142ff580b3
Aman-Shrivastav <amanshrivastav4501@gmail.com>
2024-01-24 23:18:42 +0530

    Added Changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,28 @@
 import { useState } from 'react'
-import './css/global.css';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import './index.css'
 
 function App() {
   return (
-    <div className='main-container'>
-      <Navbar/>
-      <div className='title-div'>
-        <div className='title-divs'></div>
-        <div className='title-divs'></div>
+    <>
+   <Navbar/>
+    <div className='flex flex-col items-center justify-center h-auto w-screen mb-5'>
+      <div className='h-[720px] w-screen bg-coderoom-white '>
+        <div className='title-divs text-[144px] mt-20 flex items-start justify-center '>Coderoom</div>
       </div>
-      <div className='courses-div'>
-        <div className="courses"></div>
-        <div className="courses"></div>
-        <div className="courses"></div>
-        <div className="courses"></div>
+      <div className='h-[720px] w-screen bg-coderoom-navy flex items-center gap-7 justify-center'>
+        <div className="h-[400px] w-[300px] bg-coderoom-abstract rounded-md"></div>
+        <div className="h-[400px] w-[300px] bg-coderoom-abstract rounded-md"></div>
+        <div className="h-[400px] w-[300px] bg-coderoom-abstract rounded-md"></div>
+        <div className="h-[400px] w-[300px] bg-coderoom-abstract rounded-md"></div>
       </div>
-      <div className='features-div'>
+      <div className='h-[720px] w-screen bg-coderoom-blue'>
         <div className="feature"></div>
       </div>
+      <Footer/>
     </div>
+    </>
   )
 }
 
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'coderoom-white': '#fff0e5',
+      'coderoom-navy': '#10162f',
+      'coderoom-yellow': '#ffd300',
+      'coderoom-abstract': '#eafdc6',
+      'coderoom-blue': '#3a10e5'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
